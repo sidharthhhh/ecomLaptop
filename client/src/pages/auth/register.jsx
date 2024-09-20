19d11d3ff13242c05fcca39cc90be094b5ecaaf6
sidharthhhh <swayam1044@gmail.com>
2024-09-20 09:48:44 +0530

    login and register frontend done

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,7 +1,22 @@
+import CommonForm from "@/components/common/form";
+import { registerFormControls } from "@/config";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+const initialState = {
+  userName:'',
+  email:'',
+  password:''
+}
+
 function AuthRegister() {
-  
+
+  const [formData, setFormData] = useState(initialState);
+
+  function onSubmit(){
+
+  }
+  // console.log("hello")
   return <div className="mx-auto w-full max-w-md space-y-6">
     <div className="text-center">
       <h1 className="text-3xl font-bold tracking-tight text-foreground">create new account</h1>
@@ -15,6 +30,13 @@ function AuthRegister() {
           </Link>
         </p>
     </div>
+    <CommonForm
+        formControls = {registerFormControls}
+        buttonText={'Sign up'}
+        formData = {formData}
+        setFormData={setFormData}
+        OnSubmit={onSubmit}
+    />
   </div>;
 }
 
